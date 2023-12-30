5780253938fec8c0c6f00c5cea0000292d864099
Frdl19 <laoudifarid1@gmai.com>
2023-12-30 18:15:50 +0100

    demande les nb champ et eng

--- hello.c
+++ hello.c
@@ -2,9 +2,21 @@
 #include <stdlib.h>
 
 int main() {
+    int eng,champ;
     FILE* file;
     file = fopen("creation","w+");
     fprintf(file, "Some text");
+    
+    printf("entrer le nombre des enregistrement\n");
+    do{
+        scanf("%d",&eng);
+    }while(eng<1);
+    printf("entrer le nombre des champs\n");
+    do{
+        scanf("%d",&champ);
+    }while(champ<1);
+    printf("nb enreg :%d\n",eng);
+    printf("nb des champs :%d\n",champ);
     fclose(file);
 
    return 0;
